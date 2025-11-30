6176551f17784a6807d63814f9134a3c65bd7e6c
绘念 <71483108+2200737807@users.noreply.github.com>
2025-11-30 22:31:41 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,6 +1,5 @@
 import type { UserConfig } from '~/types'
 import PostComments from "@components/PostComments.astro";
-
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "講評世界" },
