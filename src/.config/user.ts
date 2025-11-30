0278d220c0c5a9bcfeb60823fbb9dc4226476f7f
绘念 <71483108+2200737807@users.noreply.github.com>
2025-11-30 22:40:43 +0800

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -5,3 +5,21 @@ export const userConfig: Partial<UserConfig> = {
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
 }
+{
+  comments: {
+    giscus: {
+      repo: '2200737807/astro-theme-typography'
+      repoId: 'R_kgDOKy9HOQ'
+      category: 'Blog Post Comments'
+      categoryId: 'DIC_kwDOQdeeXs4CzNtE'
+      mapping: 'title'
+      strict: '0'
+      reactionsEnabled: '1'
+      emitMetadata: '1'
+      inputPosition: 'top'
+      theme: 'light'
+      lang: 'zh-CN'
+      loading: 'lazy'
+    }
+  }
+}
